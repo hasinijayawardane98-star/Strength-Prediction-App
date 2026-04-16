fa76346c1d884b0ad9efc480eaf45b7e79d992fe
hasinijayawardane98-star <hasinijayawardane98@gmail.com>
2026-04-16 00:12:15 -0400

    App5.py

--- App5.py
+++ App5.py
@@ -25,8 +25,8 @@ X_base = data.gwp_data[0][[0]].clone()
 # =========================
 # UI
 # =========================
-st.title("Concrete Strength Prediction")
-
+st.title("Concrete Strength Predictor")
+st.write("Enter mix proportions to predict strength and GWP")
 cement = st.number_input("Cement (kg/m3)", value=300, step=1, format="%d")
 fly_ash = st.number_input("Fly Ash (kg/m3)", value=50,step=1, format="%d")
 slag = st.number_input("Slag (kg/m3)", value=50, step=1, format="%d")
